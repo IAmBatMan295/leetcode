8ef519cd97b4e7784e04a0696419cb8951fcd554
IAmBatMan295 <kunjanpatel54321@gmail.com>
2026-03-10 12:30:53 +0530

    some changes

--- src/add_2numbers_linkedlist.cpp
+++ src/add_2numbers_linkedlist.cpp
@@ -10,106 +10,106 @@
 // carry being 1 even after adding all elements.
 
 struct ListNode {
-  int val;
-  ListNode *next;
-  ListNode() : val(0), next(nullptr) {}
-  ListNode(int x) : val(x), next(nullptr) {}
-  ListNode(int x, ListNode *next) : val(x), next(next) {}
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
 using node = ListNode;
 
 node *AddTwoNumbers(node *l1, node *l2) {
-  node *iterator1 = l1, *iterator2 = l2;
-  node *l3 = new node();
+    node *iterator1 = l1, *iterator2 = l2;
+    node *l3 = new node();
 
-  // new list's head(dummy).
-  //
-  node *iterator3 = l3;
+    // new list's head(dummy).
+    //
+    node *iterator3 = l3;
 
-  int carry = 0, sum = 0;
+    int carry = 0, sum = 0;
 
-  while (iterator1 != nullptr && iterator2 != nullptr) {
+    while (iterator1 != nullptr && iterator2 != nullptr) {
 
-    iterator3->next = new node();
-    iterator3 = iterator3->next;
+        iterator3->next = new node();
+        iterator3 = iterator3->next;
 
-    // create new node in the new list.
+        // create new node in the new list.
 
-    sum = iterator1->val + iterator2->val + carry;
+        sum = iterator1->val + iterator2->val + carry;
 
-    iterator3->val = sum % 10;
+        iterator3->val = sum % 10;
 
-    // store the modulus of the sum.
+        // store the modulus of the sum.
 
-    carry = sum < 10 ? 0 : 1;
+        carry = sum < 10 ? 0 : 1;
 
-    // generate carry.
+        // generate carry.
 
-    iterator1 = iterator1->next;
-    iterator2 = iterator2->next;
-  }
-  node *remaining = (iterator1 == nullptr) ? iterator2 : iterator1;
+        iterator1 = iterator1->next;
+        iterator2 = iterator2->next;
+    }
+    node *remaining = (iterator1 == nullptr) ? iterator2 : iterator1;
 
-  // determine the non-null list.
+    // determine the non-null list.
 
-  while (remaining != nullptr) {
+    while (remaining != nullptr) {
 
-    iterator3->next = new node();
-    iterator3 = iterator3->next;
+        iterator3->next = new node();
+        iterator3 = iterator3->next;
 
-    sum = remaining->val + carry;
-    iterator3->val = sum % 10;
-    carry = sum < 10 ? 0 : 1;
-    remaining = remaining->next;
-  }
+        sum = remaining->val + carry;
+        iterator3->val = sum % 10;
+        carry = sum < 10 ? 0 : 1;
+        remaining = remaining->next;
+    }
 
-  // add and append elements with carry from the remaining list to the new list.
+    // add and append elements with carry from the remaining list to the new list.
 
-  if (carry == 1)
-    iterator3->next = new node(1);
+    if (carry == 1)
+        iterator3->next = new node(1);
 
-  // handle the edgecase.
+    // handle the edgecase.
 
-  return (l3->next);
+    return (l3->next);
 
-  // return the actual head of the list instead of the dummy initializer.
+    // return the actual head of the list instead of the dummy initializer.
 }
 
 int main() {
-  node *l1 = new node(9);
-  l1->next = new node(5);
-  l1->next->next = new node(5);
-  node *l2 = new node(5);
-  l2->next = new node(5);
+    node *l1 = new node(9);
+    l1->next = new node(5);
+    l1->next->next = new node(5);
+    node *l2 = new node(5);
+    l2->next = new node(5);
 
-  node *cur = l1;
+    node *cur = l1;
 
-  while (cur != nullptr) {
-    std::cout << cur->val << " ";
-    cur = cur->next;
-  }
-  std::cout << "\n";
+    while (cur != nullptr) {
+        std::cout << cur->val << " ";
+        cur = cur->next;
+    }
+    std::cout << "\n";
 
-  cur = l2;
+    cur = l2;
 
-  while (cur != nullptr) {
-    std::cout << cur->val << " ";
-    cur = cur->next;
-  }
+    while (cur != nullptr) {
+        std::cout << cur->val << " ";
+        cur = cur->next;
+    }
 
-  // print the list before removing duplicates.
+    // print the list before removing duplicates.
 
-  std::cout << "\nafter operation :" << "\n";
+    std::cout << "\nafter operation :" << "\n";
 
-  cur = AddTwoNumbers(l1, l2);
+    cur = AddTwoNumbers(l1, l2);
 
-  while (cur != nullptr) {
-    std::cout << cur->val << " ";
-    cur = cur->next;
-  }
+    while (cur != nullptr) {
+        std::cout << cur->val << " ";
+        cur = cur->next;
+    }
 
-  std::cout << "\n";
+    std::cout << "\n";
 
-  return 0;
+    return 0;
 }
